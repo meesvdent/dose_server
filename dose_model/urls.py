a5654b7f1b68994ab951d61f353290c98c405864
meesvdent <meesvdent@gmail.com>
2020-03-14 23:16:25 +0100

    calc_conc.py setup
    https responds conc
    fixed params in view

--- dose_model/urls.py
+++ dose_model/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('update', views.update_model, name='update_model'),
+    path('calc_conc', views.calc_conc, name='calc_conc')
 ]
--- dose_model/views.py
+++ dose_model/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 import subprocess
-from django.templatetags.static import static
+import numpy as np
 from django.http import HttpResponse
+from dose_model.dose_model.models import OneCompModel
+from dose_model.dose_model.helpers import calc_dose_conc, trans_thalf_ke
 
 
 # Create your views here.
@@ -17,5 +19,28 @@ def update_model(request):
     return HttpResponse("models.py updated!")
 
 
+def calc_conc(request):
+    t = np.linspace(0, 24 * 3600, 24 * 3600)
 
+    dose = [0.160, 0.160]  # grams, seconds
+    time = [0.001, 7200]
+    molecularMass = 194.19  # Caffeine
+
+    patientMass = 75  # kg
+    DV = 0.625 * patientMass  # L/kg, for caffeine
+    ke = trans_thalf_ke(4 * 3600)
+    print(ke)
+
+    dose_conc = calc_dose_conc(dose, molecularMass, DV)
+    time_conc = [list(a) for a in zip(time, dose_conc)]
+
+    print(list(time_conc))
+
+    model = OneCompModel(time_conc, ke, 0.0055)
+    amount_unabs = model.calc_unabs(t)
+    delta_abs = model.delta_abs(amount_unabs)
+
+    X, infodict = model.integrate(t)
+
+    return HttpResponse(X)
 
