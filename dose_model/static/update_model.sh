4e9a30b09c4cbde12602169fc2e7f2c33d306987
meesvdent <meesvdent@gmail.com>
2020-03-15 14:45:26 +0100

    fixed .sh empty file

--- dose_model/static/update_model.sh
+++ dose_model/static/update_model.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-#> ./dose_model/dose_model/models.py
-curl -s https://cc8a649520a6bdf2b3a55a11f7e18a2011b66a2e@raw.githubusercontent.com/meesvdent/dose/master/models.py >> './dose_model/dose_model/models.py'
+curl -s https://cc8a649520a6bdf2b3a55a11f7e18a2011b66a2e@raw.githubusercontent.com/meesvdent/dose/master/models.py > './dose_model/dose_model/models.py'
 
-#> ./dose_model/dose_model/helpers.py
-curl -s https://cc8a649520a6bdf2b3a55a11f7e18a2011b66a2e@raw.githubusercontent.com/meesvdent/dose/master/helpers.py >>  './dose_model/dose_model/helpers.py'
+curl -s https://cc8a649520a6bdf2b3a55a11f7e18a2011b66a2e@raw.githubusercontent.com/meesvdent/dose/master/helpers.py >  './dose_model/dose_model/helpers.py'
+
