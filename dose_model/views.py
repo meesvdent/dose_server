6d0e25c074c589816358d6dd1857572583237e13
meesvdent <meesvdent@gmail.com>
2020-03-15 17:54:41 +0100

    dynamic-api-connection
    still gives output through http-response

--- dose_model/views.py
+++ dose_model/views.py
@@ -1,6 +1,5 @@
 import subprocess
-
-import requests
+import pandas
 import numpy as np
 from django.http import HttpResponse
 from dose_model.dose_model.helpers import calc_dose_conc, trans_thalf_ke
@@ -23,22 +22,31 @@ def update_model(request):
 def calc_conc(request):
 
     t = np.linspace(0, 24 * 3600, 24 * 3600)
-    compound = request.GET.get("compound")
-    dose = request.GET.get("dosage")  # grams, seconds
-    time = request.GET.get("time")
-    molecularMass = 194.19  # Caffeine
 
-    patientMass = request.GET.get("weight")  # kg
-    DV = 0.625 * patientMass  # L/kg, for caffeine
-    ke = trans_thalf_ke(4 * 3600)
-    print(ke)
+    compound = int(request.GET.get("compound"))
+    dosestring = request.GET.get("dosage")  # grams, seconds
+    dosestr = dosestring.replace(']', '').replace('[', '')
+    dose = dosestr.replace('"', '').split(",")
+    dose = list(map(float, dose))
+
+    timestring = request.GET.get("time")
+    timestr = timestring.replace(']', '').replace('[', '')
+    time = timestr.replace('"', '').split(",")
+    time = list(map(int, time))
+
+    compounds = pandas.read_csv('./dose_model/dose_model/compounds.csv', sep=";", header=0)
+
+    molecularMass = compounds.iloc[compound, 2]  # Caffeine
+
+    patientMass = float(request.GET.get("weight")) # kg
+    DV = compounds.iloc[compound, 1] * patientMass  # L/kg, for caffeine
+    ke = trans_thalf_ke(compounds.iloc[compound, 3] * 3600)
 
     dose_conc = calc_dose_conc(dose, molecularMass, DV)
     time_conc = [list(a) for a in zip(time, dose_conc)]
 
-    print(list(time_conc))
 
-    model = OneCompModel(time_conc, ke, 0.0055)
+    model = OneCompModel(time_conc, ke, compounds.iloc[compound, 4])
     amount_unabs = model.calc_unabs(t)
     delta_abs = model.delta_abs(amount_unabs)
 
